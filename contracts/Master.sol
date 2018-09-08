0b2cdce46af8a76fa68f98a242b28fc3e4f542f7
Dan Shields <danwshields@gmail.com>
2018-09-08 17:57:01 -0600

    truffle compile

--- contracts/Master.sol
+++ contracts/Master.sol
@@ -8,18 +8,18 @@ contract Master {
         //more meta data
     }
     struct IPFS_Hash {
-        string public ipfs_hash = "0x0";
-        bool private auditor_signed = false; //
+        string ipfs_hash;
+        bool auditor_signed; //
     }
 
     // need a store of the last *transaction* that a device successfully submitted - so you get a list you can scan back in the contrac twith latest -> latest -1 -> ... original.
 
     address public auditor;
     mapping(address => Device) private devices;
-    IPFS_Hash[] public ipfs_hashes;
+    IPFS_Hash[] ipfs_hashes;
     uint hash_index;
 
-    /// Create a new Master and auditor with $(_numProposals) different ipfs_hashes.
+    /// Create a new Master and auditor with $(_maxHashes) different ipfs_hashes.
     constructor(uint8 _maxHashes) public {
         auditor = msg.sender;
         ipfs_hashes.length = _maxHashes;
@@ -36,7 +36,7 @@ contract Master {
     }
 
 
-    /// Give a submit a IPFS_Hash $(to).
+    /// Submit an IPFS_Hash
     function submitHash(string newIPFS_Hash) public returns(bool){
         if(!devices[msg.sender].permissioned || hash_index >= ipfs_hashes.length-1) return false;
         ipfs_hashes[hash_index].ipfs_hash = newIPFS_Hash;
@@ -44,15 +44,15 @@ contract Master {
         return true;
     }
 
-    /// Give a submit a IPFS_Hash $(to).
+    /// Get a submited IPFS_Hash
     function getHash(uint8 index_) public returns(string){
-        return ipfs_hashes[hash_index].ipfs_hash;
+        return ipfs_hashes[index_].ipfs_hash;
     }
 
-    /// Give a submit a IPFS_Hash $(to).
-    function signAudit() internal{
+    /// Sign-off on event as a known auditor
+    function signAudit(uint8 index_) public returns(bool){
       if(msg.sender != auditor) return false;
-        auditor_signed = true;
+        ipfs_hashes[index_].auditor_signed = true;
         return true;
     }
 }
