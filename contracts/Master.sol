2b58486e63c1b7a060e1c792f0cb9caa262ffa25
Tommy Cox <tommy.cox@protonmail.com>
2018-09-08 08:46:55 -0600

    tuffle init and frist pass at contract

---
+++ contracts/Master.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.0;
+contract Master {
+
+    struct Device {
+        string name;
+        bool permissioned;
+//meta data
+    }
+    struct IPFS_Hash {
+        string ipfs_hash;
+    }
+
+    address auditor;
+    mapping(address => Device) devices;
+    IPFS_Hash[] ipfs_hashes;
+    uint index;
+
+    /// Create a new Master and auditor with $(_numProposals) different ipfs_hashes.
+    constructor(uint8 _numHashes) public {
+        auditor = msg.sender;
+        ipfs_hashes.length = _numHashes;
+        index = 0;
+    }
+
+    /// Give $(toDevice) the right to add data on this contract
+    /// May only be called by $(auditor).
+    function addDevice(address toDevice) public {
+        if (msg.sender != auditor) return;
+        devices[toDevice].permissioned = true;
+    }
+
+
+    /// Give a submit a IPFS_Hash $(to).
+    function submit(string newIPFS_Hash) public {
+        if(!devices[msg.sender].permissioned || index >= ipfs_hashes.length-1) return;
+        ipfs_hashes[index].ipfs_hash = newIPFS_Hash;
+        index++;
+    }
+    
+}
+
---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,7 @@
+var Migrations = artifacts.require("./Migrations.sol");
+var Master = artifacts.require("./Master.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(Master);
+};
